2750ee06da65c0a9c3567901b6001b49036e4459
FabioVicttor <fabinhovictor43@hotmail.com>
2020-12-06 20:15:07 -0300

    Removendo console.log

--- src/pages/Home/components/perfil/index.js
+++ src/pages/Home/components/perfil/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Card, ItemPerfil, LabelPerfil } from "./style";
-import {useAuth} from "../../../../hooks/auth"
-import Icon_CPF from "../../../../assets/icons/icon_cpf"
-import Icon_CNH from "../../../../assets/icons/icon_cnh"
-import Icon_Phone from "../../../../assets/icons/icon_phone"
-import Icon_Email from "../../../../assets/icons/icon_email"
-import Icon_Account from "../../../../assets/icons/icon_account"
+import { useAuth } from "../../../../hooks/auth";
+import Icon_CPF from "../../../../assets/icons/icon_cpf";
+import Icon_CNH from "../../../../assets/icons/icon_cnh";
+import Icon_Phone from "../../../../assets/icons/icon_phone";
+import Icon_Email from "../../../../assets/icons/icon_email";
+import Icon_Account from "../../../../assets/icons/icon_account";
 
 export default function Perfil() {
-  const {user} = useAuth()
-  console.log(user);
+  const { user } = useAuth();
   return (
     <Card>
       <div
@@ -22,7 +21,7 @@ export default function Perfil() {
           alignItems: "center",
           borderRadius: "50px",
           padding: "10px",
-          borderWidth: "5px"
+          borderWidth: "5px",
         }}
       >
         <span
@@ -31,34 +30,33 @@ export default function Perfil() {
         ></span>
       </div>
       <h1>Perfil</h1>
-      <div style={{display:"flex", flexDirection:"column"}}>
-      <ItemPerfil>
-          <Icon_Account/>
-          <LabelPerfil >Nome: </LabelPerfil>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <ItemPerfil>
+          <Icon_Account />
+          <LabelPerfil>Nome: </LabelPerfil>
           <h3>{user.name}</h3>
         </ItemPerfil>
         <ItemPerfil>
-          <Icon_CPF/>
-          <LabelPerfil >CPF: </LabelPerfil>
-          <h3 style={{margin: "0px"}}>{user.cpf}</h3>
+          <Icon_CPF />
+          <LabelPerfil>CPF: </LabelPerfil>
+          <h3>{user.cpf}</h3>
         </ItemPerfil>
         <ItemPerfil>
-          <Icon_Email/>
-          <LabelPerfil >Email: </LabelPerfil>
+          <Icon_Email />
+          <LabelPerfil>Email: </LabelPerfil>
           <h3>{user.email}</h3>
         </ItemPerfil>
         <ItemPerfil>
-          <Icon_CNH/>
-          <LabelPerfil >CNH: </LabelPerfil>
+          <Icon_CNH />
+          <LabelPerfil>CNH: </LabelPerfil>
           <h3>{user.cnh}</h3>
         </ItemPerfil>
         <ItemPerfil>
-          <Icon_Phone/>
-          <LabelPerfil >Telefone: </LabelPerfil>
+          <Icon_Phone />
+          <LabelPerfil>Telefone: </LabelPerfil>
           <h3>{user.cellphone}</h3>
         </ItemPerfil>
       </div>
-
     </Card>
   );
 }
