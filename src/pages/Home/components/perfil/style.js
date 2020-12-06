71e09ca65fee739f0397b674ab57e2e7b04a0820
Felipe Delboni Ortega <fdelb.ortega@gmail.com>
2020-12-06 19:59:07 -0300

    tela perfil - 06/12 (FABIO VAI MUDAR TUDO)

--- src/pages/Home/components/perfil/style.js
+++ src/pages/Home/components/perfil/style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Card = styled.div`
   margin-top: 5%;
@@ -6,10 +6,22 @@ export const Card = styled.div`
   height: 75%;
   width: 70%;
   border-radius: 30px;
-  display: flex;
   box-shadow: -10px 5px 20px 0px #0020389e;
+  padding: 40px;
   @media (max-width: 799px) {
     box-shadow: -10px 5px 20px 0px #0020389e;
     /* box-shadow: -5px 11px 20px 5px #00000085; */
   }
+`;
+
+export const ItemPerfil = styled.div`  
+    padding:4px; 
+    border: 20px; 
+    display: inline-flex; 
+    align-items: center;
+`;
+
+export const LabelPerfil = styled.div`  
+  margin-left: 10px;
+  margin-right: 10px;
 `;
